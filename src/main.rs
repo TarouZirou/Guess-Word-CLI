2024f9df47509d7a3c70804070a0730e7edfe387
TarouZirou <zinnnnnnnnnnnn8@gmail.com>
2025-05-27 16:49:02 +0900

    mainの修正を少し

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,19 @@
+//!文字列を当てるゲーム
+mod char;
+mod check;
+mod render;
+
+use char::mk_chars;
 use rand::Rng;
 
-//0~25の乱数をアルファベットに変換、それを5回繰り返して、文字列を返す関数
-fn mk_word() -> String {
-	let mut rng = rand::rng();
-	let rand_chars: String = (0..5)
-		.map(|_| (b'A' + rng.random_range(0..26)) as char)
-		.collect();
-	rand_chars
-}
-
-//TODO: 入力した文字列に、判定結果の色を加える関数
-fn render() {}
-
-//TODO: 文字列の一致を判定する関数
-fn decide() {}
-
 fn main() {
-	println!("Hello, world!");
+	let chars = mk_chars();
+	println!("Guess 5 Chars!");
 
-	let mut word = String::new();
-	std::io::stdin().read_line(&mut word).ok();
-	let answer = word.trim().to_string();
-
-	println!("{}", answer);
+	loop {
+		let mut word = String::new();
+		std::io::stdin().read_line(&mut word).ok();
+		let answer = word.trim().to_string();
+		println!("{}", answer);
+	}
 }
