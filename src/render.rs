c55fb092a63d2b81a0d4f61c09c9b67a247561ad
TarouZirou <zinnnnnnnnnnnn8@gmail.com>
2025-05-27 16:48:38 +0900

    テスト関数の追加

--- src/render.rs
+++ src/render.rs
@@ -1,2 +1,28 @@
+use crate::char::{char_kind, mk_chars, COLOR_TABLE};
+
 ///TODO: 入力した文字列に、判定結果の色を加える関数
-fn render() {}
+pub fn render(answer: &Vec<char>, input: &Vec<char>) {}
+
+#[test]
+fn color_print() {
+	let chars = mk_chars();
+	let strings: Vec<String> = chars.iter().map(|c| c.to_string()).collect();
+	let chars2 = mk_chars();
+	let strings2: Vec<String> = chars.iter().map(|c| c.to_string()).collect();
+	println!("{:?}", chars);
+	println!("{:?}", chars2);
+
+	let mut check: Vec<String> = Vec::new();
+	for s in 0..5 {
+		if &chars[s] == &chars2[s] {
+			check.push(String::from(format!(
+				"{}{}\x1b[0m",
+				COLOR_TABLE[char_kind::MATCH],
+				&chars[s]
+			)));
+		} else {
+			check.push(chars[s].to_string());
+		}
+	}
+	println!("{}", check.join(""));
+}
